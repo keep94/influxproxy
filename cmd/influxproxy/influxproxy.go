3e85d357083aa6b7d8851aa5b3c5205bb102e788
keep94 <keep94@gmail.com>
2016-12-06 14:55:14 -0800

    Finish proxy server.

--- cmd/influxproxy/influxproxy.go
+++ cmd/influxproxy/influxproxy.go
@@ -2,13 +2,17 @@ package main
 
 import (
 	"flag"
+	"github.com/Symantec/Dominator/lib/fsutil"
 	"github.com/Symantec/scotty/lib/apiutil"
-	"github.com/influxdata/influxdb/client/v2"
+	//	"github.com/Symantec/scotty/lib/apiutil"
+	//	"github.com/influxdata/influxdb/client/v2"
 	"github.com/influxdata/influxdb/models"
 	"github.com/influxdata/influxdb/uuid"
 	"log"
 	"net/http"
 	"net/url"
+	"os"
+	"time"
 )
 
 func setHeader(w http.ResponseWriter, r *http.Request, key, value string) {
@@ -34,23 +38,39 @@ type resultListType struct {
 }
 
 func main() {
-	backendAddr := flag.String("backendAddr", "", "backend address")
+	fConfigFile := flag.String("config", "", "config file")
 	flag.Parse()
-	cl, err := client.NewHTTPClient(
-		client.HTTPConfig{
-			Addr: *backendAddr,
-		})
-	if err != nil {
-		log.Fatal(err)
+	executer := newExecuter()
+	logger := log.New(os.Stderr, "", log.LstdFlags)
+	changeCh := fsutil.WatchFile(*fConfigFile, logger)
+	// We want to be sure we have something valid in the config file
+	// initially.
+	select {
+	case readCloser := <-changeCh:
+		if err := executer.SetupWithStream(readCloser); err != nil {
+			logger.Fatal(err)
+		}
+		readCloser.Close()
+	case <-time.After(time.Second):
+		logger.Fatal("No config file")
 	}
+	go func() {
+		for readCloser := range changeCh {
+			if err := executer.SetupWithStream(readCloser); err != nil {
+				logger.Println(err)
+			}
+			readCloser.Close()
+		}
+	}()
 	http.Handle(
 		"/query",
 		uuidHandler(
 			apiutil.NewHandler(
 				func(req url.Values) (interface{}, error) {
-					resp, err := cl.Query(
-						client.NewQuery(
-							req.Get("q"), req.Get("db"), req.Get("epoch")))
+					resp, err := executer.Query(
+						req.Get("q"),
+						req.Get("db"),
+						req.Get("epoch"))
 					if err == nil {
 						err = resp.Error()
 					}
@@ -66,9 +86,12 @@ func main() {
 						}
 					}
 					return results, nil
+
 				},
 				nil,
-			)))
+			),
+		),
+	)
 	if err := http.ListenAndServe(":8086", nil); err != nil {
 		log.Fatal(err)
 	}
